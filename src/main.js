f9276a37be53fe6aac3f9a0e76f26fdfa2d2db2c
huaponiang <2560193727@qq.com>
2023-01-15 15:50:34 +0800

    项目初始化完成

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import store from './store'
 import Vant from 'vant'
 // 2. 引入组件样式
 import 'vant/lib/index.css'
+// 动态设置rem基准值
+import 'amfe-flexible'
 // 3.注册使用vant组件库
 Vue.use(Vant)
 Vue.config.productionTip = false
